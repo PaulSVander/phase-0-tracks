9a8043a469de7db4b71b8e8e4d5b621e762397fd
Paul Vander <paulsvander@gmail.com>
2017-03-20 15:56:37 -0700

    Add multiple instances of Santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0    
   end
 
   def speak
@@ -14,6 +18,16 @@ class Santa
 
 end
 
+=begin 
 Nick = Santa.new
 Nick.speak
 Nick.eat_milk_and_cookies("sugar cookie")
+=end
+
+santas = []
+example_genders = ["male", "agender", "asexual", "cisgender", "Trans Female"]
+example_ethnicities = ["Indian", "Russian", "Italian", "Native American", "Brazilian"]
+
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
