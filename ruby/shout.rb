96251d922af19e411994ad789b2cb67cb509bb83
Paul Vander <paulsvander@gmail.com>
2017-03-20 17:06:46 -0700

    Complete 6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -12,3 +13,32 @@ end
 
 puts Shout.yell_angrily("I stubbed my toe")
 puts Shout.yelling_happily("I found 100 bucks")
+=end
+
+module Shout
+
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "!!!" + " =D"
+  end
+
+end
+
+class Orc
+  include Shout
+end
+
+class Scientist
+  include Shout
+end
+
+gorgol = Orc.new
+puts gorgol.yell_angrily("We haven't eaten in days")
+puts gorgol.yelling_happily("I found the hobbits")
+
+rick = Scientist.new
+puts rick.yell_angrily("I lost my portal gun")
+puts rick.yelling_happily("I made a new interdimensional cable box")
