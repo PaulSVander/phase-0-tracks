ff5e99a0624243e1ba0e8981859a74eef7254c89
Paul Vander <paulsvander@gmail.com>
2017-03-26 22:26:17 -0700

    Add interface

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -10,11 +10,10 @@ prompt the user for a guess
   -if they don't match, add 1 to the wrong guess counter
 if they reached the max number of guess, print lose message and quit
 
-  
 =end
 
 class Game
-  attr_reader :max_guesses, :total_guesses, :is_over
+  attr_reader :is_over, :displayed_word
   attr_accessor :target_word
 
   def initialize(target_word)
@@ -27,7 +26,7 @@ class Game
     @total_guesses = 0
     @max_guesses = target_word.length + 6
     @letters_guessed = []
-    puts "#{@displayed_word}"
+    puts "#{@displayed_word.join(" ")}"
     @is_over = false
   end
 
@@ -36,29 +35,44 @@ class Game
     correct_guess = false
 
     if @letters_guessed.include?(char)
-      puts "You already guessed that letter. Try another"
+      puts "You already guessed that letter."
+      return nil
     end
 
     index = 0
     @target_word_arr.each do |target_letter|
       if char == target_letter
         @displayed_word[index] = char
-        @correct_guess = true
+        correct_guess = true
       end
       index += 1
     end
 
-    @total_guesses += 1
-    if !correct_guess && @total_guesses == @max_guesses
+    @total_guesses += 1 unless correct_guess
+    if @total_guesses == @max_guesses
       puts "You're out of guesses!"
+      puts "#{@target_word} was the answer"
+      @is_over = true
+    elsif @displayed_word.join("") == target_word
+      puts "You won!"
+      @is_over = true
     elsif !correct_guess 
       puts "Incorrect guess!"
-    elsif displayed_word == target_word
-      @is_over = true
+      puts "#{@total_guesses} out of #{@max_guesses} guesses used"
     end
 
-    puts "#{@displayed_word}"
+    @letters_guessed << char
   end
 end
 
-Game.new("unicorn")
+
+puts "What would you like the word to be?"
+word = gets.chomp
+current_game = Game.new(word)
+
+while !current_game.is_over do
+  puts "What is your guess?"
+  guess = gets.chomp
+  current_game.guess(guess)
+  puts "#{current_game.displayed_word.join(" ")}"
+end
