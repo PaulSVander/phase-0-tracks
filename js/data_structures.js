e8aeb7e9f9f5ba5ec3a8ce75161003273cf9c891
Paul Vander <paulsvander@gmail.com>
2017-03-31 14:22:56 -0700

    Complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,10 @@ var names = ["Ed", "Billy", "Bob", "John"];
 colors.push("grey");
 names.push("Frank");
 
+var barn = {};
+
+for(i = 0; i < colors.length; i++){
+  barn[names[i]] = colors[i];
+};
+
+console.log(barn)
