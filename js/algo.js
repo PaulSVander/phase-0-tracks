c8c3255837317058513f269aeac7f97a6561b9fd
Paul Vander <paulsvander@gmail.com>
2017-04-03 00:37:19 -0700

    Complete 7.3

--- js/algo.js
+++ js/algo.js
@@ -8,9 +8,49 @@ function LongestWord(wordArray) {
   return longest;
 };
 
+function findMatch(objectOne, objectTwo)  {
+  objOneKeys = Object.keys(objectOne);
+  objTwoKeys = Object.keys(objectTwo);
 
+  for (i = 0; i < objOneKeys.length; i++) {
+    if ((objOneKeys[i] == objTwoKeys[i]) && (objectOne[objOneKeys[i]] == objectTwo[objTwoKeys[i]])) {
+      console.log("returned true");
+      return true;
+    }
+  }
+  console.log("Returned false");
+  return false;
+};
+
+function randomStrings(length) {
+  var randomArray = [];
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var text = "";
+
+  for (i = 1; i <= length; i++) {
+    for (j = 0; j <= Math.floor(Math.random() * 10) + 1; j++) {
+      text += alpha.charAt(Math.floor(Math.random() * 26));
+    }
+  randomArray.push(text);
+  text = "";
+  }
+  return randomArray;
+}
 
 var words = ["cat", "food", "thousands", "rat", "ab"];
 var otherWords = ["a phrase", "a longer phrase", "short phrase", "word"];
 console.log(LongestWord(words));
 console.log(LongestWord(otherWords));
+
+findMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+findMatch({name: "Steven", age: 54}, {name: "Tamir", years_old: 54});
+findMatch({name: "Steven", age: 54}, {name: "Tamir", age: 254});
+findMatch({alias: "Tamir", age: 54}, {name: "Tamir", age: 54});
+
+console.log(randomStrings(4));
+
+for (var count = 1; count <= 10; count++) {
+  var currentArray = randomStrings(8);
+  console.log(currentArray);
+  console.log(LongestWord(currentArray));
+}
