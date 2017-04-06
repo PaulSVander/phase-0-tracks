77b95dc752c4b3526c18dc3a78afc48a985fdc63
Paul Vander <paulsvander@gmail.com>
2017-04-05 20:42:21 -0700

    Add ability to view exercises

--- databases/workout/workout.rb
+++ databases/workout/workout.rb
@@ -10,6 +10,7 @@ create_table = <<-SQL
     weight INT
   )
 SQL
+
 workouts.execute(create_table)
 
 puts "Hello! Please select one of the following options or type 'quit'"
@@ -17,11 +18,11 @@ puts "1. Add new exercise"
 puts "2. Update existing exercise"
 puts "3. View exercise information"
 puts "4. Delete an exercise"
-user_choice = gets.chomp
+user_choice = gets.chomp.to_i
 p user_choice
 
-if user_choice == "1"
-#def add_exercise(new_exercise, reps, weight)
+=begin
+def add_exercise(new_exercise, reps, weight)
   puts "What is the name of the exercise you would like to add?"
   new_exercise = gets.chomp
   puts "How many reps did you do?"
@@ -29,9 +30,10 @@ if user_choice == "1"
   puts "How much weight did you use?"
   weight = gets.chomp
   workouts.execute("INSERT INTO exercises (name, reps, weight) VALUES (?, ?, ?)", [new_exercise, reps, weight])
-#end
 end
+=end 
 
+=begin
 def update_exercise(exercise, reps, weight)
   puts "What is the exercise you would like to update?"
   exercise = gets.chomp
@@ -39,5 +41,20 @@ def update_exercise(exercise, reps, weight)
   reps = gets.chomp
   puts "How much weight did you use?"
   weight = gets.chomp
-  workouts.execute("UPDATE workouts SET reps = ?, weight = ? WHERE name = ?", [reps, weight, exercise])
+  workouts.execute("UPDATE exercises SET reps = ?, weight = ? WHERE name = ?", [reps, weight, exercise])
+end
+=end
+=begin
+def view_exercise()
+  puts "Which exercise would you like to view?"
+  name = gets.chomp
+  display = workouts.execute("SELECT * FROM exercises WHERE name = ?", [name])
+  puts "Exercise:\t#{display[0][1]}"
+  puts "Reps:\t\t#{display[0][2]}"
+  puts "Weight:\t\t#{display[0][3]}lbs."
+end
+=end
+
+def delete_exercise()
+  
 end
