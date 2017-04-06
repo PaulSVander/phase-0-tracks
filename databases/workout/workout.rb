9d3df4eb75178b2d731a9990cfb93841f3f7b36f
Paul Vander <paulsvander@gmail.com>
2017-04-05 21:16:06 -0700

    Complete 8.5

--- databases/workout/workout.rb
+++ databases/workout/workout.rb
@@ -1,6 +1,6 @@
 require 'sqlite3'
 
-workouts = SQLite3::Database.new("workouts.db")
+$workouts = SQLite3::Database.new("workouts.db")
 
 create_table = <<-SQL
   CREATE TABLE IF NOT EXISTS exercises(
@@ -10,51 +10,88 @@ create_table = <<-SQL
     weight INT
   )
 SQL
+$workouts.execute(create_table)
 
-workouts.execute(create_table)
-
-puts "Hello! Please select one of the following options or type 'quit'"
-puts "1. Add new exercise"
-puts "2. Update existing exercise"
-puts "3. View exercise information"
-puts "4. Delete an exercise"
-user_choice = gets.chomp.to_i
-p user_choice
-
-=begin
-def add_exercise(new_exercise, reps, weight)
+def add_exercise()
   puts "What is the name of the exercise you would like to add?"
   new_exercise = gets.chomp
   puts "How many reps did you do?"
   reps = gets.chomp
   puts "How much weight did you use?"
   weight = gets.chomp
-  workouts.execute("INSERT INTO exercises (name, reps, weight) VALUES (?, ?, ?)", [new_exercise, reps, weight])
+  $workouts.execute("INSERT INTO exercises (name, reps, weight) VALUES (?, ?, ?)", [new_exercise, reps, weight])
 end
-=end 
-
-=begin
-def update_exercise(exercise, reps, weight)
+ 
+def update_exercise()
   puts "What is the exercise you would like to update?"
+  list_exercises
   exercise = gets.chomp
   puts "How many reps did you do?"
   reps = gets.chomp
   puts "How much weight did you use?"
   weight = gets.chomp
-  workouts.execute("UPDATE exercises SET reps = ?, weight = ? WHERE name = ?", [reps, weight, exercise])
+  $workouts.execute("UPDATE exercises SET reps = ?, weight = ? WHERE name = ?", [reps, weight, exercise])
 end
-=end
-=begin
+
 def view_exercise()
   puts "Which exercise would you like to view?"
+  list_exercises
   name = gets.chomp
-  display = workouts.execute("SELECT * FROM exercises WHERE name = ?", [name])
+  display = $workouts.execute("SELECT * FROM exercises WHERE name = ?", [name])
   puts "Exercise:\t#{display[0][1]}"
   puts "Reps:\t\t#{display[0][2]}"
   puts "Weight:\t\t#{display[0][3]}lbs."
 end
-=end
+
+def view_all()
+  display = $workouts.execute("SELECT * FROM exercises")
+  display.each do |exercise|
+    p exercise
+    puts "Exercise:\t#{exercise[1]}"
+    puts "Reps:\t\t#{exercise[2]}"
+    puts "Weight:\t\t#{exercise[3]}lbs."
+    puts "------------------------"
+  end
+end
 
 def delete_exercise()
-  
+  puts "Which exercise would you like to delete?"
+  name = gets.chomp
+  $workouts.execute("DELETE FROM exercises WHERE name = ?", [name])
+end
+
+def list_exercises()
+  display = $workouts.execute("SELECT name FROM exercises")
+  display.each do |exercise|
+    puts exercise
+  end
+end
+
+
+user_choice = nil
+while user_choice != "quit" do
+  puts "Hello! Please select one of the following options using it's number or type 'quit'"
+  puts "1. Add new exercise"
+  puts "2. Update existing exercise"
+  puts "3. View an exercise"
+  puts "4. View all exercises"
+  puts "5. Delete an exercise"
+  user_choice = gets.chomp
+
+  case user_choice
+  when "1"
+    add_exercise
+  when "2"
+    update_exercise
+  when "3"
+    view_exercise
+  when "4"
+    view_all
+  when "5"
+    delete_exercise
+  when "quit"
+    puts "Exiting program"
+  else
+    puts "Invalid input"
+  end
 end
