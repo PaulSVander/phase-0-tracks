c936d703b2d40760acd9781003ed12c1d092ffad
Paul Vander <paulsvander@gmail.com>
2017-04-07 14:47:48 -0700

    Add method to create new user

--- databases/workout/workout.rb
+++ databases/workout/workout.rb
@@ -2,3 +2,25 @@ require_relative 'workout_methods'
 
 db = create_database
 
+
+puts "Welcome to the workout tracker!"
+valid_user = false
+while !valid_user
+  puts "Please enter your username"
+  username_input = gets.chomp
+  if !is_existing_user(db, username_input)
+    puts "This user does not exist. Would you like to add them to the table?"
+    answer = gets.chomp.downcase
+    if answer == "yes"
+      add_user(db, username_input)
+      valid_user = true
+    else
+      puts "User was not added"
+    end
+  end
+  valid_user = true
+end
+
+puts "Welcome #{username_input}. Select from one of the following options"
+puts "1. Add a user
+      2. "
--- databases/workout/workout_methods.rb
+++ databases/workout/workout_methods.rb
@@ -1,13 +1,13 @@
 require 'sqlite3'
 
+#initializes database with tables
 def create_database
   db = SQLite3::Database.new("workout.db")
   
   create_users_table = <<-SQL
     CREATE TABLE IF NOT EXISTS users(
       id INTEGER PRIMARY KEY,
-      first_name VARCHAR(255),
-      last_name VARCHAR(255),
+      username VARCHAR(255),
       age INT
     )
   SQL
@@ -24,12 +24,34 @@ def create_database
   create_join_table = <<-SQL
     CREATE TABLE IF NOT EXISTS exercises_users(
       exercise_id INT,
-      user_id INT
+      user_id INT,
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (exercise_id) REFERENCES exercises(id)
     )
   SQL
 
   db.execute(create_users_table)
   db.execute(create_exercises_table)
   db.execute(create_join_table)
+  db
 
+end
+
+# Check to see if user already exists
+def is_existing_user(db, username)
+  username_arr = db.execute("SELECT username FROM users")
+  username_arr.each do |user|
+    if user[0] == username
+      return true
+    end
+  end
+  return false
+end
+
+# add a user to the table
+def add_user(db, username)
+  puts "What is the age of the new user?"
+  age = gets.chomp
+  db.execute("INSERT INTO users (username, age) VALUES (?, ?)", [username, age])
+  puts "#{username} was added to the table"
 end
