9a3e49f2f88c6f07fcf6e48ac12c86f63eed423e
Paul Vander <paulsvander@gmail.com>
2017-04-07 16:45:41 -0700

    Add method to view an exercise

--- databases/workout/workout.rb
+++ databases/workout/workout.rb
@@ -17,10 +17,35 @@ while !valid_user
     else
       puts "User was not added"
     end
+   else
+    valid_user = true 
   end
-  valid_user = true
 end
 
-puts "Welcome #{username_input}. Select from one of the following options"
-puts "1. Add a user
-      2. "
+user_id = db.execute("SELECT id FROM users WHERE username=?", [username_input])
+user_id = user_id[0][0]
+p user_id
+puts "Welcome #{username_input}. Select from one of the following options or enter quit."
+puts "1. Add an exercise"
+puts "2. Update an exercise"
+puts "3. View an exercises information"
+puts "4. View all exercise information"
+puts "5. Delete an exercise"
+user_selection = gets.chomp
+
+case user_selection
+when "1"
+  puts "What is the name of the exercise you want to add?"
+  exercise = gets.chomp
+  if is_existing_exercise(db, exercise)
+    puts "#{exercise} is already in the database."
+  else
+    add_exercise(db, exercise)
+    puts "#{exercise} was added to the database."
+  end
+when "2"
+  update_exercise(db, user_id)
+when "3"
+  view_info(db, user_id)
+end
+
--- databases/workout/workout_methods.rb
+++ databases/workout/workout_methods.rb
@@ -12,17 +12,11 @@ def create_database
     )
   SQL
 
-  create_exercises_table = <<-SQL
-    CREATE TABLE IF NOT EXISTS exercises(
-      id INTEGER PRIMARY KEY,
-      name VARCHAR(255),
-      reps INT,
-      weight INT
-    )
-  SQL
-
   create_join_table = <<-SQL
     CREATE TABLE IF NOT EXISTS exercises_users(
+      id INTEGER PRIMARY KEY,
+      reps INT,
+      weight INT,
       exercise_id INT,
       user_id INT,
       FOREIGN KEY (user_id) REFERENCES users(id),
@@ -30,9 +24,18 @@ def create_database
     )
   SQL
 
+  create_exercises_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS exercises(
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(255) 
+    )
+  SQL
+
+
+
+  db.execute(create_join_table)
   db.execute(create_users_table)
   db.execute(create_exercises_table)
-  db.execute(create_join_table)
   db
 
 end
@@ -48,10 +51,64 @@ def is_existing_user(db, username)
   return false
 end
 
+def is_existing_exercise(db, name)
+  exercises_arr = db.execute("SELECT name FROM exercises")
+  exercises_arr.each do |exercise|
+    if exercise[0] == name
+      return true
+    end
+  end
+  return false
+end
+
 # add a user to the table
 def add_user(db, username)
   puts "What is the age of the new user?"
   age = gets.chomp
   db.execute("INSERT INTO users (username, age) VALUES (?, ?)", [username, age])
   puts "#{username} was added to the table"
+end
+
+def add_exercise(db, name)
+  db.execute("INSERT INTO exercises (name) VALUES (?)", [name])
+end
+
+# Update reps and weight for a users exercise
+def update_exercise(db, user_id)
+  # Prompts user for an exercise until a valid response is received
+  puts "What exercise would you like to update?"
+  exercise = gets.chomp
+  until is_existing_exercise(db, exercise) do
+    puts "Invalid exercise. Please retry."
+    exercise = gets.chomp
+  end
+  exercise_id = db.execute("SELECT id FROM exercises WHERE name=?", exercise)
+  exercise_id = exercise_id[0][0]
+
+  # If not data exists for this user with this exercise, creates a new row
+  if db.execute("SELECT * FROM exercises_users WHERE user_id=? AND exercise_id=?", [user_id, exercise_id]).empty?
+    db.execute("INSERT INTO exercises_users (reps, weight, exercise_id, user_id) VALUES (0, 0, ?, ?)", [exercise_id, user_id])
+  end
+  
+  puts "How many reps did you do?"
+  reps = gets.chomp
+  puts "How much weight did you use?"
+  weight = gets.chomp
+  db.execute("UPDATE exercises_users SET reps=?, weight=? WHERE exercise_id=? AND user_id=?", [reps, weight, exercise_id, user_id])
+end
+
+# View a users info for a specific exercise
+def view_info(db, user_id)
+  puts "Which exercise would you like to view?"
+  exercise = gets.chomp
+  until is_existing_exercise(db, exercise) do
+    puts "Invalid exercise. Please retry."
+    exercise = gets.chomp
+  end
+  exercise_id = db.execute("SELECT id FROM exercises WHERE name=?", exercise)
+  exercise_id = exercise_id[0][0]
+
+  results = db.execute("SELECT * FROM exercises_users WHERE user_id=? AND exercise_id=?", [user_id, exercise_id])
+  puts "You last did #{results[0][1]} reps of #{exercise} at #{results[0][1]} pounds"
+
 end
