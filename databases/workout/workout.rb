79783f8842f3cac7fc2b611c50c1f0bbf7699e56
Paul Vander <paulsvander@gmail.com>
2017-04-07 14:05:51 -0700

    Create method for initializing database with tables

--- databases/workout/workout.rb
+++ databases/workout/workout.rb
@@ -1,41 +1,4 @@
-require 'sqlite3'
+require_relative 'workout_methods'
 
-$workouts = SQLite3::Database.new("workouts.db")
+db = create_database
 
-create_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS exercises(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    reps INT,
-    weight INT
-  )
-SQL
-$workouts.execute(create_table)
-
-user_choice = nil
-while user_choice != "quit" do
-  puts "Hello! Please select one of the following options using it's number or type 'quit'"
-  puts "1. Add new exercise"
-  puts "2. Update existing exercise"
-  puts "3. View an exercise"
-  puts "4. View all exercises"
-  puts "5. Delete an exercise"
-  user_choice = gets.chomp
-
-  case user_choice
-  when "1"
-    add_exercise
-  when "2"
-    update_exercise
-  when "3"
-    view_exercise
-  when "4"
-    view_all
-  when "5"
-    delete_exercise
-  when "quit"
-    puts "Exiting program"
-  else
-    puts "Invalid input"
-  end
-end
--- databases/workout/workout_methods.rb
+++ databases/workout/workout_methods.rb
@@ -1,54 +1,35 @@
-def add_exercise()
-  puts "What is the name of the exercise you would like to add?"
-  new_exercise = gets.chomp
-  puts "How many reps did you do?"
-  reps = gets.chomp
-  puts "How much weight did you use?"
-  weight = gets.chomp
-  $workouts.execute("INSERT INTO exercises (name, reps, weight) VALUES (?, ?, ?)", [new_exercise, reps, weight])
-end
- 
-def update_exercise()
-  puts "What is the exercise you would like to update?"
-  list_exercises
-  exercise = gets.chomp
-  puts "How many reps did you do?"
-  reps = gets.chomp
-  puts "How much weight did you use?"
-  weight = gets.chomp
-  $workouts.execute("UPDATE exercises SET reps = ?, weight = ? WHERE name = ?", [reps, weight, exercise])
-end
+require 'sqlite3'
 
-def view_exercise()
-  puts "Which exercise would you like to view?"
-  list_exercises
-  name = gets.chomp
-  display = $workouts.execute("SELECT * FROM exercises WHERE name = ?", [name])
-  puts "Exercise:\t#{display[0][1]}"
-  puts "Reps:\t\t#{display[0][2]}"
-  puts "Weight:\t\t#{display[0][3]}lbs."
-end
+def create_database
+  db = SQLite3::Database.new("workout.db")
+  
+  create_users_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS users(
+      id INTEGER PRIMARY KEY,
+      first_name VARCHAR(255),
+      last_name VARCHAR(255),
+      age INT
+    )
+  SQL
 
-def view_all()
-  display = $workouts.execute("SELECT * FROM exercises")
-  display.each do |exercise|
-    p exercise
-    puts "Exercise:\t#{exercise[1]}"
-    puts "Reps:\t\t#{exercise[2]}"
-    puts "Weight:\t\t#{exercise[3]}lbs."
-    puts "------------------------"
-  end
-end
+  create_exercises_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS exercises(
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(255),
+      reps INT,
+      weight INT
+    )
+  SQL
 
-def delete_exercise()
-  puts "Which exercise would you like to delete?"
-  name = gets.chomp
-  $workouts.execute("DELETE FROM exercises WHERE name = ?", [name])
-end
+  create_join_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS exercises_users(
+      exercise_id INT,
+      user_id INT
+    )
+  SQL
 
-def list_exercises()
-  display = $workouts.execute("SELECT name FROM exercises")
-  display.each do |exercise|
-    puts exercise
-  end
+  db.execute(create_users_table)
+  db.execute(create_exercises_table)
+  db.execute(create_join_table)
+
 end
